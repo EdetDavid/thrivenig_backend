c8d27f9c52c8469537fa2f04b01ce6e9c845f776
Dvooskid <dvooskid1234@gmail.com>
2024-06-19 22:49:00 +0100

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
-
-from .views import ReportClaim, ContactMail, NewsletterSubscription
-from django.urls import path, include
+from django.urls import path
+from .views import ReportClaim, ContactMail, NewsletterSubscription, SubmitCv
+from django.urls import path
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contact/', ContactMail.as_view(), name='contact'),
     path('newsletter/', NewsletterSubscription.as_view(),
          name='newsletter-subscribe'),
+    path('submit-cv/', SubmitCv.as_view(), name='submit_cv'),
 
 ]
